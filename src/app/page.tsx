1c8c06ec2e59c60a45cfcdcb7fda0443314ab827
Vidit Khandelwal <viditk17@gmail.com>
2023-09-09 17:02:17 -0400

    updated cal.com link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
             </div>
           </Link>
           <Link
-            href="https://read.cv/viditkhandelwal"
+            href="https://cal.com/viditkhandelwal"
             className="group p-2 lg:p-4 col-span-3 lg:col-span-2 flex flex-row space-x-2 justify-around items-center bg-white rounded-lg transition-all duration-200 ease-in-out outline-offset-[#0000FF] hover:ring hover:ring-gray-300 hover:shadow-2xl hover:shadow-[#0000FF] text-2xl lg:text-4xl"
           >
             <PiVideoCameraDuotone />
