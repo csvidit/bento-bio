287d2c680aea4c116675bd80f91d207c7e172768
Vidit Khandelwal <viditk17@gmail.com>
2023-09-25 16:43:18 -0400

    updated calendar booking link

--- src/components/CalLink.tsx
+++ src/components/CalLink.tsx
@@ -4,7 +4,7 @@ import { PiVideoCameraDuotone } from "react-icons/pi";
 const CalLink = () => {
   return (
     <Link
-      href="https://cal.com/viditkhandelwal"
+      href="https://calendly.com/viditkhandelwal"
       className="group p-2 lg:p-4 col-span-3 lg:col-span-2 flex flex-row space-x-2 justify-around items-center bg-white rounded-lg transition-all duration-200 ease-in-out outline-offset-[#0000FF] hover:ring hover:ring-gray-300 hover:shadow-2xl hover:shadow-[#0000FF] text-2xl lg:text-4xl"
     >
       <PiVideoCameraDuotone />
@@ -12,7 +12,7 @@ const CalLink = () => {
         <div className="text-gray-800 text-base">
           Let&apos;s schedule a meeting!
         </div>
-        <div className="text-gray-500 text-xs">via Cal.com</div>
+        <div className="text-gray-500 text-xs">via Calendly</div>
       </div>
     </Link>
   );
