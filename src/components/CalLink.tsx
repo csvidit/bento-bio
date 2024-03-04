41cf224b877b0d021adcfa6e7b71c362a83f9f39
csvidit <63170797+csvidit@users.noreply.github.com>
2024-03-04 17:27:29 -0500

    update calendar link

--- src/components/CalLink.tsx
+++ src/components/CalLink.tsx
@@ -4,7 +4,7 @@ import { PiVideoCameraDuotone } from "react-icons/pi";
 const CalLink = () => {
   return (
     <Link
-      href="https://calendly.com/viditkhandelwal"
+      href="https://cal.com/viditkhandelwal"
       className="group p-2 lg:p-4 col-span-3 lg:col-span-2 flex flex-row space-x-2 justify-around items-center bg-white rounded-lg transition-all duration-200 ease-in-out outline-offset-[#0000FF] hover:ring hover:ring-gray-300 hover:shadow-lg hover:shadow-accent text-2xl lg:text-4xl"
     >
       <PiVideoCameraDuotone />
